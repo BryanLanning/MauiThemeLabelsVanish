6695de9ff63f72f651acd832083a99e08402f0eb
Bryan Lanning <blanning@owenind.com>
2024-09-23 13:53:00 -0500

    Add project files.

---
+++ MauiThemeLabelsVanish/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiThemeLabelsVanish
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
